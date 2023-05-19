ea6480484c21580a89c0fb3017e01513792f7043
Sarparaat <115228211+Sarparaat@users.noreply.github.com>
2023-05-19 13:32:32 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function dragstart_handler(ev) {
+    // Add the target element's id to the data transfer object
+    ev.dataTransfer.setData("application/my-app", ev.target.id);
+    ev.dataTransfer.effectAllowed = "move";
+  }
+  function dragover_handler(ev) {
+    ev.preventDefault();
+    ev.dataTransfer.dropEffect = "move";
+  }
+  function drop_handler(ev) {
+    ev.preventDefault();
+    const data = ev.dataTransfer.getData("text/plain");
+    ev.target.appendChild(document.getElementById(data));
+
+  }
